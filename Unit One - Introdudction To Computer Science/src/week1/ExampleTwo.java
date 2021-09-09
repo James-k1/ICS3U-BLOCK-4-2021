cbdf610b0458aff0fd65d27330b157d44fd5ea01
James Kurian <jkurian1@bayviewglen.ca>
2021-09-09 14:50:42 -0400

    test

---
+++ Unit One - Introdudction To Computer Science/src/week1/ExampleTwo.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class ExampleTwo {
+    public static void main(String[] args) {
+        System.out.println("A"); // "A" with enter
+        System.out.print("B"); // "BCD"
+        System.out.print("C");
+        System.out.println("D");
+        System.out.println("E"); //"E"
+
+    }
+    
+}
