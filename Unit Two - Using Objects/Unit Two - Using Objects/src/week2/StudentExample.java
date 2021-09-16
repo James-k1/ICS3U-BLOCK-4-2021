906619a9928564efd600a8c0fd80f7bf91bdf6ef
James Kurian <jkurian1@bayviewglen.ca>
2021-09-16 14:58:13 -0400

    Unit two

---
+++ Unit Two - Using Objects/Unit Two - Using Objects/src/week2/StudentExample.java
@@ -0,0 +1,25 @@
+package week2;
+
+public class StudentExample {
+    public static void main(String[] args) {
+        Student fred = new Student("Fred", "11874", 10);
+        Student max = new Student("Max", "11875", 10);
+        Student muriel = fred;
+        muriel.increaseGrade();
+
+        fred = null;
+        max.addTest(94);
+        max.addTest(87);
+        max.addTest(98);
+        max.addTest(67);
+        max.displayAverage();
+        
+
+
+    
+        
+    }
+    
+
+    
+}
